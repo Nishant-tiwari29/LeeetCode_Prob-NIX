7481127c7549d27012fba03edf035eca33cdfa11
Nishant Tiwari <nishanttiwari.nt9@gmail.com>
2026-09-23 09:17:41 +0530

    Time: 0 ms (100%), Space: 42.6 MB (98.01%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            
+            if (ch != ' ') {
+                length++; 
+            } else if (length > 0) {
+                
+                break;
+            }
+        }
+        
+        return length;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
