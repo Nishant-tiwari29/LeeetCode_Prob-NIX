c99d310d2c07a4fdd397779796c067e1d00dd9bd
Nishant Tiwari <nishanttiwari.nt9@gmail.com>
2026-09-25 22:08:21 +0530

    Time: 275 ms (85.43%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,9 @@
+SELECT max(salary) AS SecondHighestSalary
+From Employee
+Where salary < 
+(Select max(salary) 
+FROM Employee);
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
